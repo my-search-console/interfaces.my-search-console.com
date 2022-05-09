c9f6b934cce3e2d9b6104d46d0db59432c7fa6f2
Kevin Marques <marques.developpement@gmail.com>
2022-05-09 13:32:48 +0200

    feat: add mur product

--- src/entities/BuilderWebsiteEntity.ts
+++ src/entities/BuilderWebsiteEntity.ts
@@ -1,6 +1,12 @@
 export enum BuilderWebsiteTheme {
   PURITY = "purity",
+  MUR = "mur",
 }
+
+export const BuilderProductsToTheme: { [x: string]: BuilderWebsiteTheme } = {
+  ["770119"]: BuilderWebsiteTheme.PURITY,
+  ["770561"]: BuilderWebsiteTheme.MUR,
+};
 
 export interface BuilderWebsiteEntity {
   id: string;
