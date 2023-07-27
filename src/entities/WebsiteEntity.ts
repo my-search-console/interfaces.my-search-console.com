892c15b81d881ca0c7e95aa1401cc58cc2fb7375
Marques Kevin <marques.developpement@gmail.com>
2023-07-27 10:52:18 +0200

    feat(WebsiteEntity): ajout de yandex et bing

--- src/entities/WebsiteEntity.ts
+++ src/entities/WebsiteEntity.ts
@@ -1,6 +1,8 @@
 export class WebsiteEntity {
   id: string;
   search_console_domain: string | null;
+  yandex_domain: string | null;
+  bing_domain: string | null;
   image: string;
   sitemap: string | null;
   already_activated: boolean;
