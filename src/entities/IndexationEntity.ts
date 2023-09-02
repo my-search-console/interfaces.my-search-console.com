394b8c2dce3146ff78e48a7953bbe10d7eda1b61
Marques Kevin <marques.developpement@gmail.com>
2023-09-02 19:02:39 +0200

    feat(IndexationQueueEntity): ajout d'une nouvelle entité

--- src/entities/IndexationEntity.ts
+++ src/entities/IndexationEntity.ts
@@ -3,3 +3,14 @@ export type IndexationSettingsEntity = {
   indexation_auto_activated: boolean;
   sources: Array<"google" | "yandex" | "naver" | "bing">
 }
+
+export type IndexationQueueEntity = {
+  id: string;
+  fk_website_id: string;
+  page: string;
+  status: "queue" | "done";
+  sources: Array<"google" | "yandex" | "naver" | "bing">
+  created_at: Date;
+  indexed_at: Date;
+  page_updated_at: Date;
+}
