7adcae5230830e272771bbe0dd0aa563335556b5
Marques Kevin <marques.developpement@gmail.com>
2023-09-03 10:06:47 +0200

    fix: PageEntity ajout d'une colonne in_sitemap_last_modified_at

--- src/entities/PageEntity.ts
+++ src/entities/PageEntity.ts
@@ -19,6 +19,7 @@ export class PageEntity {
   updated_at: Date;
   fk_website_id: string;
   request_indexing_at: Date | null;
+  in_sitemap_last_modified_at:  Date | null;
 }
 
 export class PageIndexingEntity {
