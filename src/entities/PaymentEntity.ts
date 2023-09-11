5d15e4b9a4994061affd10655c47fb03ba254ff6
Marques Kevin <marques.developpement@gmail.com>
2023-09-11 23:51:48 +0200

    feat(PaymentEntity): mise à jour du type

--- src/entities/PaymentEntity.ts
+++ src/entities/PaymentEntity.ts
@@ -1,10 +1,19 @@
+export enum PaymentPlansEntity {
+    indexation = "indexation",
+    keywords = "keywords",
+    analytics = "analytics",
+    all = "all"
+}
+
 export type PaymentEntity = {
-    pfk_website_id: string;
+    id: string;
     fk_user_id: string;
     cancellation_effective_date: Date | null;
     subscription_id: string;
     cancel_url: string;
     update_url: string;
+    created_at: Date;
     paddle_user_id: string;
+    plan: PaymentPlansEntity
 };
   
