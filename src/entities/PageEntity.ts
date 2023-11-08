8c805e7adc80c42c1f2dab3010554726d2afaa5a
Kevin Marques <marques.developpement@gmail.com>
2023-11-08 16:48:54 +0100

    feat: ajout de nouveaux états d'indexation

--- src/entities/PageEntity.ts
+++ src/entities/PageEntity.ts
@@ -1,25 +1,41 @@
 export enum IndexationType {
-  NOT_INDEXED = 'not-indexed',
-  INDEXED = 'indexed',
-  NOT_PROCESSED = 'not-processed',
-  INDEXING = 'indexing'
-}
+  NOT_INDEXED = "not-indexed",
+  INDEXED = "indexed",
+  NOT_PROCESSED = "not-processed",
+  INDEXING = "indexing",
 
+  "server-error-5xx" = "server-error-5xx",
+  "redirect-error" = "redirect-error",
+  "url-blocked-by-robots.txt" = "url-blocked-by-robots.txt",
+  "url-marked-noindex" = "url-marked-noindex",
+  "soft-404" = "soft-404",
+  "blocked-due-to-unauthorized-request-401" = "blocked-due-to-unauthorized-request-401",
+  "not-found-404" = "not-found-404",
+  "blocked-due-to-access-forbidden-403" = "blocked-due-to-access-forbidden-403",
+  "url-blocked-due-to-other-4xx-issue" = "url-blocked-due-to-other-4xx-issue",
+  "blocked-by-page-removal-tool" = "blocked-by-page-removal-tool",
+  "crawled-currently-not-indexed" = "crawled-currently-not-indexed",
+  "discovered-currently-not-indexed" = "discovered-currently-not-indexed",
+  "alternate-page-with-proper-canonical-tag" = "alternate-page-with-proper-canonical-tag",
+  "duplicate-without-user-selected-canonical" = "duplicate-without-user-selected-canonical",
+  "duplicate-google-chose-different-canonical-than-user" = "duplicate-google-chose-different-canonical-than-user",
+  "page-with-redirect" = "page-with-redirect",
+}
 
 export enum IndexationSourceType {
-  google = 'google',
-  yandex = 'yandex',
-  bing = 'bing',
-  naver = 'naver' 
+  google = "google",
+  yandex = "yandex",
+  bing = "bing",
+  naver = "naver",
 }
-  
+
 export class PageEntity {
   url: string;
   indexation_state: IndexationType;
   updated_at: Date;
   fk_website_id: string;
   request_indexing_at: Date | null;
-  in_sitemap_last_modified_at:  Date | null;
+  in_sitemap_last_modified_at: Date | null;
 }
 
 export class PageIndexingEntity {
