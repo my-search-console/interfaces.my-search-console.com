38f1ea26c3a5aacbce937547c884e7770f489412
Marques Kevin <marques.developpement@gmail.com>
2024-01-19 15:55:51 +0100

    feat: ajout des roles dans users to websites

--- src/entities/UserToWebsiteEntity.ts
+++ src/entities/UserToWebsiteEntity.ts
@@ -1,5 +1,12 @@
+export enum UserToWebsiteRoles {
+  admin = "admin",
+  write = "write",
+  read = "read",
+}
+
 export class UserToWebsiteEntity {
   id: string;
   fk_website_id: string;
   fk_user_id: string;
+  role: UserToWebsiteRoles
 }
