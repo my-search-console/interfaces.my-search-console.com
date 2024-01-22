8c19503bb68dd6f119a3aca846687793d070b563
Marques Kevin <marques.developpement@gmail.com>
2024-01-22 09:56:27 +0100

    feat(UserEntity): ajout d'un type

--- src/entities/UserEntity.ts
+++ src/entities/UserEntity.ts
@@ -1,6 +1,12 @@
+import { UserToWebsiteRoles } from "./UserToWebsiteEntity";
+
 export class UserEntity {
   id: string;
   email: string;
   language: string;
   created_at: Date;
+}
+
+export type UserWithRoleEntity = UserEntity & {
+  role: UserToWebsiteRoles
 }
